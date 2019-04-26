dd43479aae2e8975f24ae304c15b567636b6002d
Michael Libois <libois.contact@gmail.com>
2019-04-26 11:07:30 +0200

    1

---
+++ MyCollection/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyCollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
