48a975f42e1252396298345532b3b33d48139496
Michael Libois <libois.contact@gmail.com>
2019-04-30 16:59:08 +0200

    30-04 16:59

---
+++ MyCollection/Auth/MyCollectionClaimTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCollection.Auth
+{
+    public class MyCollectionClaimTypes
+    {
+
+        public static List<string> ClaimsList { get; set; } = new List<string> { "Delete Pie", "Add Pie", "Age for ordering" };
+    }
+}
